08882f82eea4004dec1785a06ff8479f82394ae1
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-01-31 21:25:11 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nipoo::Application.config.secret_token = 'a55543fdeb1dbbfe26343d23b58716d9bd19e655769e0aa2ba35ef8e0988250fe3221ca3c45ae68cb34f40bbc7fb3774ca037fabf38139aeca2a0bfc99c335a5'
