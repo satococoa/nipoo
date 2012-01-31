6022f8841c622bc5fafff95e5fcb55692081d895
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-01-31 21:54:04 +0900

    default route

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Nipoo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'reports#index'
 
   # See how all your routes lay out with "rake routes"
 
