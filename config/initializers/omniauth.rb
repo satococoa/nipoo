c5122b54fc20e27c5f723ef2905d42fa40b7f5bf
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-01 01:22:12 +0900

    use SettingsLogic to set api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,4 @@
-unless Rails.env.production?
-  ENV['GITHUB_KEY'] = 'cb9176883591da3759fd'
-  ENV['GITHUB_SECRET'] = 'c19270b8e7c042c1def000d46e2ccf895c2f5a8e'
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, Settings.github.key, Settings.github.secret
 end
