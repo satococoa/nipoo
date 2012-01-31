93099533be9348233e9752e383112d0cc7f097ab
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-01 01:11:42 +0900

    delete API KEY for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
-if Rails.env.production?
-  ENV['GITHUB_KEY'] = 'c26ed2c604c968091462'
-  ENV['GITHUB_SECRET'] = '29b610428f0f6ec3aecd5e7c31067f0317c8dcc5'
-else
+unless Rails.env.production?
   ENV['GITHUB_KEY'] = 'cb9176883591da3759fd'
   ENV['GITHUB_SECRET'] = 'c19270b8e7c042c1def000d46e2ccf895c2f5a8e'
 end
