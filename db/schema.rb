332a38bdbf6dd1a8ff26b58e96db17fbc0e0bdef
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-02 02:08:37 +0900

    r g model comment report_id:integer user_id:integer body:text && rake:db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120131125132) do
+ActiveRecord::Schema.define(:version => 20120201170811) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "report_id"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "reports", :force => true do |t|
     t.date     "date"
---
+++ spec/fabricators/comment_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator(:comment) do
+  report_id 1
+  user_id 1
+  body "MyText"
+end
