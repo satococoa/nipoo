0c5f96e182a80de12dd70423430a5d34384caa41
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-05 17:43:31 +0900

    add some basic validations closes #9

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :report
+
+  validates :body, :presence => true
+  validates :report_id, :presence => true
+  validates :user_id, :presence => true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,10 @@ class Report < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
+  validates :date, :presence => true
+  validates :body, :presence => true
+  validates :user_id, :presence => true
+
   scope :date, lambda {|date| where(date: date) }
   scope :user, lambda {|user| where(user_id: user) }
 
