ded31e6cf925a55977d48083ad7cf6f06a7f8306
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-05 22:11:49 +0900

    configure for guard-rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,4 +29,7 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
