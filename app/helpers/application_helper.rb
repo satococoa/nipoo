4cb2ac49501af05704a6b7da7f04cd3348280a21
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-10 00:31:45 +0900

    modify markdown helper(still commented out)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,18 @@
 module ApplicationHelper
   def markdown(text)
     simple_format(text)
-    # begin
-      # options = {
-        # :autolink => true,
-        # :hard_wrap => true
-      # }
-      # @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
-      # @markdown.render(text).html_safe
-    # rescue => e
-      # Rails.logger.error [e.class, e.message].join(' ')
-      # "変換に失敗しました。。。\n"+text.html_safe
-    # end
+=begin
+    begin
+      options = {
+        :autolink => true,
+        :hard_wrap => true
+      }
+      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
+      @markdown.render(text).html_safe
+    rescue => e
+      Rails.logger.error [e.class, e.message].join(' ')
+      simple_format("変換に失敗しました。。。\n\n"+text)
+    end
+=end
   end
 end
