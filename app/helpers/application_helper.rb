31fa7560a2cab82d39dd333e9937b484cb6dc502
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-09 21:54:12 +0900

    rescue markdown exception

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
+# coding: utf-8
 module ApplicationHelper
   def markdown(text)
-    options = {
-      :autolink => true,
-      :hard_wrap => true
-    }
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
-    @markdown.render(text).html_safe
+    begin
+      options = {
+        :autolink => true,
+        :hard_wrap => true
+      }
+      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
+      @markdown.render(text).html_safe
+    rescue => e
+      Rails.logger.error [e.class, e.message].join(' ')
+      "変換に失敗しました。。。\n"+text.html_safe
+    end
   end
 end
