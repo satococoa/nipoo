600d101348cb03e13efcb739e86f7bc07a0c7921
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-09 22:03:30 +0900

    temporarily disable markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,17 @@
 # coding: utf-8
 module ApplicationHelper
   def markdown(text)
-    begin
-      options = {
-        :autolink => true,
-        :hard_wrap => true
-      }
-      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
-      @markdown.render(text).html_safe
-    rescue => e
-      Rails.logger.error [e.class, e.message].join(' ')
-      "変換に失敗しました。。。\n"+text.html_safe
-    end
+    text
+    # begin
+      # options = {
+        # :autolink => true,
+        # :hard_wrap => true
+      # }
+      # @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(options))
+      # @markdown.render(text).html_safe
+    # rescue => e
+      # Rails.logger.error [e.class, e.message].join(' ')
+      # "変換に失敗しました。。。\n"+text.html_safe
+    # end
   end
 end
