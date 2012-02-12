9307c9e7f5c41956557252de81d44125a65ffd79
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-02-12 14:27:28 +0900

    make secret_token secure

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Nipoo::Application.config.secret_token = 'a55543fdeb1dbbfe26343d23b58716d9bd19e655769e0aa2ba35ef8e0988250fe3221ca3c45ae68cb34f40bbc7fb3774ca037fabf38139aeca2a0bfc99c335a5'
+Nipoo::Application.config.secret_token = Settings.secret_token
